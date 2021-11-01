306995dbb2927e83dce91aa3bef4bcda338eec52
ara-ta3 <2089153+ara-ta3@users.noreply.github.com>
2021-11-01 22:46:32 +0900

    Add scalatraboostrap

--- src/main/scala/com/ru/waka/app/ScalatraBootstrap.scala
+++ src/main/scala/com/ru/waka/app/ScalatraBootstrap.scala
@@ -2,8 +2,10 @@ package com.ru.waka.app
 
 import javax.servlet.ServletContext
 import org.scalatra._
+import com.ru.waka.app.controllers.UsersController
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
+    context.mount(new UsersController, "/*")
   }
 }
